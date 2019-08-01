b15488d45ed62fbf456d203711245e8ee471b19d
Denis Farrer <denis.farrer@gmail.com>
2019-08-01 18:46:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ string=string.toLowerCase()
 console.log (string);
 }
 function sayHiToGrandma(string) {
-if (string.toLowerCase = string) {
-return 'I can't hear you!';}
+if (string.toLowerCase() = string) {
+return "I can't hear you!";}
 }
